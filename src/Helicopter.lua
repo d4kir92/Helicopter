5b1124f4e74a64b2948596925d1e33858744cb28
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-04-21 09:36:54 +0200

    Retail + Mists

--- src/Helicopter.lua
+++ src/Helicopter.lua
@@ -1,10 +1,15 @@
 print("AH-64 locked and loaded!")
-
+local spinningIds = {}
+spinningIds[101546] = true -- MISTS
+spinningIds[322729] = true -- RETAIL
 local f = CreateFrame("Frame")
-f:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START")
-f:SetScript("OnEvent", function(self, event, ...)
-	local unit, spellGUID, spellID = ...
-    if (spellID == 101546 and unit == "player") then 
-        PlaySoundFile("Interface\\AddOns\\Helicopter\\heli.mp3");
+f:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_START", "player")
+f:SetScript(
+    "OnEvent",
+    function(self, event, ...)
+        local _, _, spellID = ...
+        if spinningIds[spellID] then
+            PlaySoundFile("Interface\\AddOns\\Helicopter\\heli.mp3")
+        end
     end
-end)
+)
